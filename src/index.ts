e534b0fc2ab59fff982de4fee27ccd756f2f8cb4
guy-car <guil.crn@gmail.com>
2025-06-11 15:15:48 -0400

    simple elysia app with middleware

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,46 @@
-import { Elysia } from "elysia";
+import swagger from "@elysiajs/swagger";
+import { Elysia, status } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const PORT = 3000
+
+// Simple API key store (use database in production)
+const validApiKeys = new Set(['api-key-123', 'api-key-456'])
+
+// add a system for issuing api key
+
+const protectedRoutes = new Elysia()
+  .derive({ as: 'local' }, (request) => {
+    return { user: { name: 'guillaume', role: 'admin' }, isAuthenticated: true }
+  })
+  .onBeforeHandle({ as: 'local' }, (request) => {
+    const headers = request.headers
+    const apiKey = headers['x-api-key']
+
+    if (!apiKey) return status(401)
+
+    const isAuthenticated = validApiKeys.has(apiKey)
+
+    if (!isAuthenticated) return status(401)
+
+    if (request.user.role !== 'admin') return status(401)
+  })
+  .get('/protected', () => {
+    return { message: 'access granted! now try /protected-with-context' }
+  })
+  .get('/protected-with-context', ({ user, isAuthenticated }) => {
+    return { message: 'Access granted!', user: user, authenticated: isAuthenticated }
+  })
+// .derive() // exists to add CONTEXT to requests.
+
+const app = new Elysia()
+  .use(swagger())
+  .get('/', () => {
+    return { message: "this is public hello world" }
+  })
+  .use(protectedRoutes)
+  .listen(PORT)
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
 );
+
